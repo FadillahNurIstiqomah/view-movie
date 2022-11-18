ec06d4c1d2f15f97aefab90c14b47a65514bcd96
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2022-11-18 07:51:59 +0700

    update navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,17 @@ const Footer = () => {
         <footer className="mt-5">
             <div className="text-center footer_style" style={{backgroundColor: '#2b2b2a'}}>
                 <div className="logo_socmed">
-                    <a href='https://movielist-react.vercel.app/'><i className="fa-brands fa-youtube fa-lg"></i></a>
-                    <a href='https://movielist-react.vercel.app/'><i className="fa-brands fa-instagram fa-lg"></i></a>
-                    <a href='https://movielist-react.vercel.app/'><i className="fa-brands fa-facebook-square fa-lg"></i></a>
-                    <a href='https://movielist-react.vercel.app/'><i className="fa-brands fa-twitter fa-lg"></i></a>
+                    <a href='https://youtube.com/'><i className="fa-brands fa-youtube fa-lg"></i></a>
+                    <a href='https://instagram.com/'><i className="fa-brands fa-instagram fa-lg"></i></a>
+                    <a href='https://facebook.com/'><i className="fa-brands fa-facebook-square fa-lg"></i></a>
+                    <a href='https://twitter.com/'><i className="fa-brands fa-twitter fa-lg"></i></a>
                 </div>
                 <div className="footer_text">
                     <a href="https://movielist-react.vercel.app/">Conditions of Use</a>
                     <a href="https://movielist-react.vercel.app/">Privacy & Policy</a>
-                    <a href="https://movielist-react.vercel.app/">Press Room</a>
+                    <a href="https://movielist-react.vercel.app/">About Us</a>
                 </div>
-                <p className="mt-2 text-white">Copyright &copy; 2022 MovieList. All Right Reserved.</p>
+                <p className="mt-2 text-white">Copyright &copy; 2022 View. All Right Reserved.</p>
             </div>
         </footer>
     )
